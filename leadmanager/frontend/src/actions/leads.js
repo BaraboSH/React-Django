2f85ed832934a21323fb28c92e184402cb970496
Nikita Trembitskyi <ntre@diceus.com>
2019-04-30 17:45:21 +0300

    Added auth on django

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { createMessage, returnErrors } from './messages';
 
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
 
@@ -10,27 +11,33 @@ export const getLeads = () => dispatch => {
                 type: GET_LEADS,
                 payload: res.data
             })
-        }).catch(err => console.error(err));
+        }).catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 }
 
 //DeleteLead
 export const deleteLead = (id) => dispatch => {
     axios.delete(`/api/leads/${id}/`)
         .then(res => {
+            dispatch(createMessage({
+                deleteLead: 'Lead deleted'
+            }));
             dispatch({
                 type: DELETE_LEAD,
                 payload: id
-            })
-        }).catch(err => console.error(err));
+            });
+        }).catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 }
 
 //AddLead
 export const addLead = (lead) => dispatch => {
     axios.post(`/api/leads/`, lead)
         .then(res => {
+            dispatch(createMessage({
+                addLead: 'Lead Added'
+            }));
             dispatch({
                 type: ADD_LEAD,
                 payload: res.data
-            })
-        }).catch(err => console.error(err));
+            });
+        }).catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 }
